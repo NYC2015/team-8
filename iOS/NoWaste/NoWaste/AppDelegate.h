dc0939be17bdcf63e1269b0d19d9b8352fde6ed3
reynoldsjay <jayrey000@gmail.com>
2015-10-17 03:45:17 -0400

    iOS project

---
+++ iOS/NoWaste/NoWaste/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NoWaste
+//
+//  Created by Jay Reynolds on 10/17/15.
+//  Copyright © 2015 com.reynoldsJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/NoWaste/NoWaste/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NoWaste
+//
+//  Created by Jay Reynolds on 10/17/15.
+//  Copyright © 2015 com.reynoldsJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
