c7b3cc1dd74329efd49bf7ce241018bda9cd5214
exp0nge <mdislamwork@gmail.com>
2015-10-16 22:59:11 -0400

    Add basic models for donors and snap users

--- donor/admin.py
+++ donor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from donor import models
 
 # Register your models here.
+admin.site.register(models.DonorProfile)
+admin.site.register(models.Store)
+admin.site.register(models.Food)
