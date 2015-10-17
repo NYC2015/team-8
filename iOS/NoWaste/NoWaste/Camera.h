3627fdc52f1d7559f22124ba16bd09b80f78c460
reynoldsjay <jayrey000@gmail.com>
2015-10-17 05:41:43 -0400

    Layouts

---
+++ iOS/NoWaste/NoWaste/Camera.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  NoWaste
+//
+//  Created by Jay Reynolds on 10/17/15.
+//  Copyright © 2015 com.reynoldsJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Camera : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
+- (IBAction)takePhoto:  (UIButton *)sender;
+- (IBAction)selectPhoto:(UIButton *)sender;
+
+
+@end
+
---
+++ iOS/NoWaste/NoWaste/Camera.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NoWaste
+//
+//  Created by Jay Reynolds on 10/17/15.
+//  Copyright © 2015 com.reynoldsJay. All rights reserved.
+//
+
+#import "Camera.h"
+
+@interface Camera ()
+
+@end
+
+@implementation Camera
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
