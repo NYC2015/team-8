acdcdf26cdd0693aea4bd632e7cf0c4b64a7f1ca
reynoldsjay <jayrey000@gmail.com>
2015-10-17 00:08:34 -0400

    Random codes that aren't in db.

---
+++ utils.py
@@ -0,0 +1,11 @@
+from donor.models import Store
+import random
+
+def generateCouponCode():
+    code = None
+    while code is None
+        x = random.randint(0,16777215)
+        coupon = Coupon(code=x)
+        if coupon.objects:
+            coupon = code
+    return code
