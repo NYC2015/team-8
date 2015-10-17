9bb78231cc0f3b490803c1b6b6b01bdf75f2d29f
exp0nge <mdislamwork@gmail.com>
2015-10-17 00:27:07 -0400

    Add login for snap users

--- feed_the_children/urls.py
+++ feed_the_children/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'register/', views.register, name='register'),
+    url(r'login/', views.user_login, name='login'),
 ]
