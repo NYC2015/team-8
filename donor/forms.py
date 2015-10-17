1efb9ec0df7fbda65256949ce1af33c086f61eb1
exp0nge <mdislamwork@gmail.com>
2015-10-16 23:14:17 -0400

    Add forms for models

---
+++ donor/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from donor import models
+
+
+class DonorProfileForm(forms.ModelForm):
+    class Meta:
+        model = models.DonorProfile
+
+
+class StoreForm(forms.ModelForm):
+    class Meta:
+        model = models.Store
+        fields = ('name', 'address', 'state', 'zip')
+
+
+class FoodForm(forms.ModelForm):
+    class Meta:
+        model = models.Food
+        fields = ('name', 'quantity', 'weight', 'category')
